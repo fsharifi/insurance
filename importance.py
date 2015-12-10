c3072260bad7312f7cdf164d2259d401f26aec54
turkerfan <esamath@gmail.com>
2015-12-10 18:33:10 -0500

    categorical to numerical is added

--- importance.py
+++ importance.py
@@ -1,5 +1,6 @@
 print(__doc__)
 
+import utils
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,13 +14,16 @@ train=train.fillna(0)
 # print(train.isnull().sum())
 
 train=train.values
-#train=train[0:100,:]
+# train=train[0:100,:]
 # Build a classification task using 3 informative features
 indeces=(range(train.shape[1]-1))
-indeces=list(set(indeces)-set([0,2]))
+indeces=list(set(indeces)-set([0]))#removing ID
 # print(indeces)
 # print(train.shape[1])
-# exit(0)
+changed=utils.categoricalToNumerical(train[:,2])
+print("number of categories of column 2:",changed[1])
+train[:,2]=changed[0]
+
 X, y = train[:,indeces] , train[:,-1]
 y=list(map(np.int32,y))
 
--- utils.py
+++ utils.py
@@ -3,4 +3,11 @@ import numpy as np
 
 def categoricalToNumerical(v):
     dict={}
-    biggest
+    biggest=0
+    res=[]
+    for i in v:
+        if not i in dict:
+            dict[i]=biggest
+            biggest+=1
+        res.append(dict[i])
+    return np.array(res),biggest
